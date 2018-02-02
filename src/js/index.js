f20e21b7c28def404758e34aac2cbf7bbba99453
Mireille Bobbert <mlfbobbert@gmail.com>
2018-02-02 12:08:48 +0100

    added function Deck and function Card create HTML

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,36 @@
 'use strict'; // Whole-script strict mode syntax
+
+function Deck (card) {
+	this.card = card;
+
+
+	this.info = function() {
+		return this.card;
+    }
+
+
+
+}
+
+function Card(suit, rank, value, reverse, x, y) {
+    this.suit = suit;
+    this.rank = rank;
+    this.value = value;
+    this.reverse = reverse;
+    this.x = x;
+    this.y = y;
+
+    this.info = function() {
+		return this.suit + ' ' + this.rank + ' ' + this.reverse;
+    }
+
+    this.createHTML = function() {
+		$('.deck').append(
+            $('<div id="' + this.suit + '">')
+            .addClass('spades-7'))
+            .css('left', this.x * 64 + 'px')
+            .css('top', this.y * 64 + 'px')
+        }
+
+}
+
