060baa86b458e04911185499e5bbbb76ff06f294
Mireille Bobbert <mlfbobbert@gmail.com>
2018-02-02 15:52:44 +0100

    added card values

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,31 @@
 'use strict'; // Whole-script strict mode syntax
 
-function Deck (card) {
-	this.card = card;
+function Deck(card) {
+  this.card = card;
 
 
-	this.info = function() {
-		return this.card;
-    }
+  this.info = function () {
+    return this.card;
+  }
 
 }
 
-function generateDeck(){
+function generateDeck() {
   var deck = [];
 
   var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
-  var values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
-  for(var s = 0; s < suits.length; s++){
-      for(var v = 0; v < values.length; v++){
+  var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
+
+  for (var s = 0; s < suits.length; s++) {
+    for (var v = 0; v < ranks.length; v++) {
       //   {
       //     'suit': suits[s],
       //     'value': values[v],
       //     'reversed': false
       // }
 
-        deck.push(new Card(suits[s], values[v]));
-      }
+      deck.push(new Card(suits[s], ranks[v]));
+    }
   }
 
   return deck;
@@ -35,16 +36,16 @@ function shuffleDeck(deck) {
 
   // While there are elements in the array
   while (counter > 0) {
-      // Pick a random indexx§
-      var index = Math.floor(Math.random() * counter);
+    // Pick a random indexx§
+    var index = Math.floor(Math.random() * counter);
 
-      // Decrease counter by 1
-      counter--;
+    // Decrease counter by 1
+    counter--;
 
-      // And swap the last element with it
-      var temp = deck[counter];
-      deck[counter] = deck[index];
-      deck[index] = temp;
+    // And swap the last element with it
+    var temp = deck[counter];
+    deck[counter] = deck[index];
+    deck[index] = temp;
   }
 
   return deck;
@@ -53,37 +54,42 @@ function shuffleDeck(deck) {
 
 
 function Card(suit, rank) {
-    this.suit = suit;
-    this.rank = rank;
-    this.value = 0;
-    this.reverse = true;
+  this.suit = suit;
+  this.rank = rank;
 
-    this.info = function() {
-		return this.suit + ' ' + this.rank + ' ' + this.reverse;
-    }
+  if (this.rank === "jack" ||
+    this.rank === "queen" ||
+    this.rank === "king") {
+    this.value = 10;
+  } else if (this.rank === "ace") {
+    this.value = 11;
+  } else {
+    this.value = parseInt(this.rank)
+  };
 
-    this.createHTML = function() {
-		  $('.deck').append(
-            $('<div id="' + this.suit + '">')
-            .addClass('spades-7'))
-    }
+  this.reverse = true;
 
-    this.createHTML_card_image = function() {
-      console.log("whatever")
-      $('.player_area').append (
-        $('<div class="playerscard">')
-        .addClass(this.suit + "-" + this.rank)
-      )
+  this.info = function () {
+    return this.suit + ' ' + this.rank + ' ' + this.reverse;
+  }
 
-    }
+  this.createHTML = function () {
+    $('.deck').append(
+      $('<div id="' + this.suit + '">')
+      .addClass('spades-7'))
+  }
 
-//function Hand(value, card) {
-  //this.value = value;
-  //this.card = card;
-
-
-
-
+  this.createHTML_card_image = function () {
+    console.log("whatever")
+    $('.player_area').append(
+      $('<div class="playerscard">')
+      .addClass(this.suit + "-" + this.rank)
+    )
 
   }
 
+  //function Hand(value, card) {
+  //this.value = value;
+  //this.card = card;
+
+}
